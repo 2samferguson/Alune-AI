0b489bbe1221bebbea8e0933d6d0b9fa48e50aa4
sam <2samferguson@gmail.com>
2023-01-28 06:43:43 -0500

    Initial commit

---
+++ model.js
@@ -0,0 +1,80 @@
+function randomize() {
+  for (let i = 0; i < 5; i++) {
+    team[i] = rolechamps[i][Math.floor(Math.random()*rolechamps[i].length)];
+    enemy[i] = rolechamps[i][Math.floor(Math.random()*rolechamps[i].length)];}}
+
+function compute(id, r, side){
+  te = [team, enemy][side];
+  en = [enemy, team][side];
+  s = cf*basedeltas[id]+roledeltas[id][r];
+  for (let i = 0; i < 5; i++) {
+    if (i != r) {s += synergies[id][te[i]];}
+    s += counters[id][en[i]];}
+  return 100*ll(s)-50}
+
+function onsubmit(x) {
+  qval = grab("form"+x).value.toLowerCase().split(" ");
+  qchamp = namedict[qval[0]];
+  qrole = poproles[qchamp];
+  if (qval.length > 1){qrole = roledict[qval[1]];}
+  [team, enemy][x][qrole] = qchamp;
+  if (qchamp == undefined){[team, enemy][x][qrole] = null;}
+  grab("form"+x).value = null;
+  grab("form"+x).placeholder = "";
+  display()}
+
+function predict(p) {
+  for (let i = 0; i < 5; i++) {
+    p += basedeltas[team[i]] + roledeltas[team[i]][i];
+    p -= basedeltas[enemy[i]] + roledeltas[enemy[i]][i];
+    for (let j = 0; j < 5; j++) {
+      if (i != j) {p += synergies[team[i]][team[j]]/2;}
+      if (i != j) {p -= synergies[enemy[i]][enemy[j]]/2;}
+      p += counters[team[i]][enemy[j]];}}
+  grab("bar1").textContent = (100*ll(p)).toFixed(2);
+  grab("bar2").textContent = (100*ll(-p)).toFixed(2);
+  grab("bar1").style.width = String((490*ll(p)).toFixed(2))+"px";
+  grab("bar2").style.left = String((490*ll(p)).toFixed(2))+"px";
+  grab("bar2").style.width = String((490*ll(-p)).toFixed(2))+"px";}
+
+function display(){
+  for (let j = 0; j < 2; j++) {
+    side = [team, enemy][j]
+    for (let i = 0; i < 5; i++) {
+      if (side[i] == null){
+        grab("x"+i+" y"+(3*j+1)).textContent = null;
+        grab("x"+i+" y"+(3*j+2)).src = "Assets/"+role[i]+".png";
+        grab("x"+i+" y"+(3*j+3)).textContent = null;
+        continue;}
+
+      score = compute(side[i], i, j)
+      showscore = ["+ ", "- "][+(score < 0)]+Math.abs(score).toFixed(2)
+      grab("x"+i+" y"+(3*j+1)).textContent = names[side[i]];
+      grab("x"+i+" y"+(3*j+2)).src = ci+names[side[i]]+".png";
+      grab("x"+i+" y"+(3*j+3)).textContent = showscore;
+      grab("x"+i+" y"+(3*j+3)).style.color = ["lime", "red"][+(score < 0)^j];}}
+
+  grab("filter").textContent=["Show all Champions","Filter by Role"][showall];
+  grab("cf").textContent=["Restore Default Scoring","Flatten Base Scores"][cf];
+
+  function compare(a, b){return compute(b, myrole, 0) - compute(a, myrole, 0);}
+  function argsort(){return champlist.sort(compare);}
+  champlist = rolechamps[myrole];
+  if (showall == 1) {champlist = [...Array(162).keys()];}
+  order = argsort();
+  buildscroll();
+  predict(0);
+
+  for (let i = 0; i < champlist.length; i++) {
+    j = order[i]
+    score = compute(j, myrole, 0)
+    showscore = ["+ ", "- "][+(score < 0)]+Math.abs(score).toFixed(2)
+    grab("xx1 yy"+i).src = "Assets/"+role[myrole]+".png";
+    grab("xx2 yy"+i).src = ci+names[j]+".png";
+    grab("xx3 yy"+i).textContent = names[j];
+    grab("xx4 yy"+i).textContent = showscore;
+    grab("xx4 yy"+i).style.color = ["lime", "red"][+(score < 0)];
+    grab("xx5 yy"+i).textContent = (100*ll(basedeltas[j])).toFixed(2);
+    grab("xx6 yy"+i).textContent = (100*
+
+    games[j][myrole]/total).toFixed(2);}}
---
+++ script.js
@@ -0,0 +1,90 @@
+function ll(x){return 1/(1+Math.exp(-x));}
+function onclick(elem, func) {elem.addEventListener("click", func);}
+function grab(classname){return document.getElementsByClassName(classname)[0];}
+function add(type, parentclass, childclass, top, left){
+  parent = grab(parentclass);
+  child = document.createElement(type);
+  child.style.top = String(top)+"px";
+  child.style.left = String(left)+"px";
+  child.className = childclass;
+  parent.appendChild(child);}
+
+async function load() {
+  patch = grab("dropdown").value
+  mainurl = "https://www.aluneaiserver.com/";
+
+  poproles = await (await fetch(mainurl+'data/setup/poproles')).json();
+  rolechamps = await (await fetch(mainurl+'data/setup/rolechamps')).json();
+  names = await (await fetch(mainurl+'data/setup/names')).json();
+  roledict = await (await fetch(mainurl+'data/setup/roledict')).json();
+  namedict = await (await fetch(mainurl+'data/setup/namedict')).json();
+  total = await (await fetch(mainurl+'data/'+patch+'/total')).json();
+  games = await (await fetch(mainurl+'data/'+patch+'/games')).json();
+  roledeltas = await (await fetch(mainurl+'data/'+patch+'/roledeltas')).json();
+  basedeltas = await (await fetch(mainurl+'data/'+patch+'/basedeltas')).json();
+  synergies = await (await fetch(mainurl+'data/'+patch+'/synergies')).json();
+  counters = await (await fetch(mainurl+'data/'+patch+'/counters')).json();
+
+  roledeltas[null] = [0, 0, 0, 0, 0]; basedeltas[null] = 0;
+  for (let i = 0; i < 162; i++) {synergies[i][null] = 0; counters[i][null] = 0;}
+  zeros = Array(162).fill(0); zeros[null] = 0;
+  counters[null] = zeros; synergies[null] = zeros;
+
+  rolechamps[3] = [9, 17, 19, 25, 29, 49, 50, 51, 54,
+  70, 77, 106, 116, 129, 133, 137, 138, 148, 157, 158]
+  roledeltas[129][3] = 0; namedict["del"] = null;
+
+  grab("words6").textContent = total+" Matches Analyzed"
+  display()}
+
+role = ["Top", "Jungle", "Middle", "Bottom", "Support"];
+titles = ["", "Name", "Score", "Base", "Pick"];
+
+nl = [null, null, null, null, null];
+myrole = 0; showall = 0; cf = 1;
+team = [null, null, null, null, null];
+enemy = [null, null, null, null, null];
+
+ci = "Assets/Champion_icon_"
+
+for (let i = 0; i < 5; i++) {
+  add("div", "main", "x"+i+" y1 slab txt", 100, i*100);
+  add("img", "main", "x"+i+" y2 big", 150, i*100);
+  add("div", "main", "x"+i+" y3 slab txt", 250, i*100);
+  add("div", "main", "x"+i+" y4 slab txt", 350, i*100);
+  add("img", "main", "x"+i+" y5 big", 400, i*100);
+  add("div", "main", "x"+i+" y6 slab txt", 500, i*100);
+  add("button", "main", "x"+i+" y7 rb big", 0, (i+6)*100);
+  add("div", "main", "x"+i+" y8 slab txt", 100, (i+6)*100);
+  grab("x"+i+" y2").src = "Assets/"+role[i]+".png";
+  grab("x"+i+" y5").src = "Assets/"+role[i]+".png";
+  grab("x"+i+" y7").style.backgroundImage = "url('"+"Assets/"+role[i]+".png"+"')";
+  grab("x"+i+" y8").textContent = titles[i];
+  onclick(grab("x"+i+" y7"), function() {myrole = i ; display()});}
+
+function buildscroll() {
+  grab("scroll").remove()
+  add("div", "main", "scroll", 150, 600);
+  for (let i = 0; i < champlist.length; i++) {
+    add("img", "scroll", "small xx1 yy"+i, 50*i, 0);
+    add("img", "scroll", "small xx2 yy"+i, 50*i, 50);
+    add("div", "scroll", "slab txt xx3 yy"+i, 50*i, 100);
+    add("div", "scroll", "slab txt xx4 yy"+i, 50*i, 200);
+    add("div", "scroll", "slab txt xx5 yy"+i, 50*i, 300);
+    add("div", "scroll", "slab txt xx6 yy"+i, 50*i, 400);
+    grab("xx1 yy"+i).src = "Assets/Specialist.png";
+    grab("xx2 yy"+i).src = "Assets/NoChampionSquare.png";}}
+
+grab("form0").placeholder = "Eg. 'Aphelios' or 'aphel' or 'aph bot'";
+
+function onenter(x) {if (event.keyCode == 13) {onsubmit(x);}}
+grab("form0").addEventListener("keypress", function() {onenter(0)});
+grab("form1").addEventListener("keypress", function() {onenter(1)});
+
+onclick(grab("random"), function() {randomize(); display()});
+onclick(grab("filter"), function() {showall = 1 - showall; display()});
+onclick(grab("cf"), function() {cf = 1 - cf; display()});
+onclick(grab("reset"), function() {team = [...nl]; enemy = [...nl]; display()});
+onclick(grab("switch"), function() {[team, enemy] = [enemy, team]; display()})
+
+load()
